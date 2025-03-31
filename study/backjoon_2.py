aae14f7aabf11b39e0481a65e39c1e5253067826
chawj <hanmy3541@gmail.com>
2025-03-31 09:41:23 +0900

    new file:   study/backjoon_1.py 	new file:   study/backjoon_2.py

---
+++ study/backjoon_2.py
@@ -0,0 +1,19 @@
+
+a, b= map(int, input().split())
+#더하기
+print(a+b)
+#뺴기기
+print(a-b)
+#곱하기기
+print(a*b)
+#나누기
+print(a/b)
+#사칙연산산
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+#꼬마 정민
+a,b,c= map(int, input().split())
+print(a+b+c)
